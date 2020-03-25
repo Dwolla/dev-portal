d034a062a3ee500cc962613e2f039791f3eb9126
Stephen Ausman <stephen@dwolla.com>
2020-03-25 10:00:36 -0500

    Refactor stories

--- stories/Badge.stories.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import Badge from "../components/base/Badge";
-
-storiesOf("Badge", module).add("Default", () => <Badge text="BADGETEXT" />);
