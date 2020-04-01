4e7e454f9a0aea6ca1a5abbeff5f77b4760d806d
Stephen Ausman <stephen@dwolla.com>
2020-04-01 09:00:44 -0500

    Update Footer

--- components/breakpoints.ts
+++ components/breakpoints.ts
@@ -1,3 +1,4 @@
 export const maxWidth = (breakpoint) => `max-width: ${breakpoint + 1}px`;
+export const minWidth = (breakpoint) => `min-width: ${breakpoint}px`;
 
 export const BREAKPOINT_DESKTOP = 1024;
