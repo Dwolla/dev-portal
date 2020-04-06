924e60f40ed929994504cdb376cea86df60ce80c
Stephen Ausman <stephen@dwolla.com>
2020-04-06 09:15:07 -0500

    Test onClick in FilterTabs

--- components/base/__tests__/FilterTabs.test.tsx
+++ components/base/__tests__/FilterTabs.test.tsx
@@ -1,18 +1,17 @@
 import renderer from "react-test-renderer";
-import ReactTestUtils from "react-dom/test-utils";
-import FilterTabs from "../FilterTabs";
+import FilterTabs, { TabStyle } from "../FilterTabs";
 
-test("Filter Tabs", () => {
+const TABS = ["GET BUILDING", "PREREQUISITES", "FEATURES"];
+
+test("FilterTabs", () => {
   const setState = jest.fn();
-  const tree = renderer
-    .create(
-      <FilterTabs
-        tabs={["GET BUILDING", "PREREQUISITES", "FEATURES"]}
-        filter={null}
-        setFilter={setState}
-      />
-    )
-    .toJSON();
-  ReactTestUtils.Simulate.click(tree);
-  expect(tree).toMatchSnapshot();
+  const tree = renderer.create(
+    <FilterTabs tabs={TABS} filter={null} setFilter={setState} />
+  );
+  tree.root.findAllByType(TabStyle).map((t) => t.props.onClick());
+
+  expect(setState.mock.calls.map((c) => c)).toEqual(
+    [null, ...TABS].map((t) => [t])
+  );
+  expect(tree.toJSON()).toMatchSnapshot();
 });
