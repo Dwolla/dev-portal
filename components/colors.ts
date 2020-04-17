4e5d7fd099d8296bc94819a43b4e7fc76644a362
Connor Finholt <cfinholt@dwolla.com>
2020-04-17 09:43:26 -0500

    WEB-608: Adding Selector Dropdown Component (#31)
    * WEB-608: Adding Selector Dropdown Component
    
    * WEB-608: Removing accidental comment
    
    * WEB-608: Fixing CheckMarkIcon naming

--- components/colors.ts
+++ components/colors.ts
@@ -8,6 +8,7 @@ export const GREY_6 = "#52627b";
 export const GREY_7 = "#3e4e67";
 export const GREY_8 = "#354153";
 export const GREY_9 = "#2e3745";
+export const GREY_10 = "#cfcff5";
 
 // Text Colors
 export const HEADLINE_TEXT = "#2a2d38";
@@ -20,12 +21,15 @@ export const PURPLE_PRIMARY_BUTTON = "#5e4e76";
 export const PURPLE_PRIMARY_HOVER = "#6a5885";
 export const PURPLE_PRIMARY_ACTIVE = "#524467";
 export const PURPLE_DARK = "#2d2d47";
+export const PURPLE_DARKER = "#242438";
 export const PINK_SECONDARY_BUTTON = "#cb4976";
 export const PINK_SECONDARY_HOVER = "#e24d80";
 export const PINK_SECONDARY_ACTIVE = "#be406c";
 export const WHITE_PRIMARY = "#ffffff";
 
 // Code Block Colors
+export const CODE_BLOCK_BUTTON = "#383c4c";
+export const CODE_BLOCK_BLACK = "#1d1e24";
 export const CODE_BLOCK_YELLOW = "#e6db74";
 export const CODE_BLOCK_PINK = "#f92672";
 export const CODE_BLOCK_GREEN = "#a6e22e";
---
+++ public/images/check-mark-icon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const CheckMarkIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 10 7"
+    width="10px"
+    height="7px"
+  >
+    <path
+      d="M0 3l3.1 3L9 0"
+      stroke="#52627B"
+      fill="none"
+      fillRule="evenodd"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default CheckMarkIcon;
