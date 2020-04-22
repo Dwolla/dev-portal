d4f251154574ea336fdba2000070a05fd46f8a29
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-04-22 10:28:35 -0500

    Web 631/Banner component (#36)
    * Updated FilterTabs component to use Select
    
    * Added Banner component
    
    * Added bottom padding to SelectWrapper

--- components/base/FilterTabs.tsx
+++ components/base/FilterTabs.tsx
@@ -1,66 +1,10 @@
 import React from "react";
-import Dropdown from "react-dropdown";
 import styled from "@emotion/styled";
-import arrowDown from "../../public/images/arrow-down-icon.svg";
-import arrowUp from "../../public/images/arrow-up-icon.svg";
-import { GREY_4, WHITE_PRIMARY, PURPLE_DARK, ORANGE_PRIMARY } from "../colors";
+import Select from "./select/Select";
+import { GREY_4, WHITE_PRIMARY, ORANGE_PRIMARY } from "../colors";
 import { POPPINS } from "../typography";
 import { minWidth, maxWidth, BREAKPOINT_MOBILE } from "../breakpoints";
 import classnames from "../../modules/classnames";
-
-type Props = {
-  tabs: string[];
-  filter: string;
-  setFilter: Function;
-};
-
-export default function FilterTabs({ tabs, filter, setFilter }: Props) {
-  const dropdownOptions = [
-    { value: "ALL", label: "ALL", className: "option" },
-    ...tabs.map((item) => ({
-      value: item,
-      label: item,
-      className: "option",
-    })),
-  ];
-  const defaultOption = filter || dropdownOptions[0];
-
-  return (
-    <div>
-      <TabsContainerStyle>
-        <TabStyle
-          className={classnames("tab", { "is-active": filter === null })}
-          onClick={() => setFilter(null)}
-        >
-          ALL
-        </TabStyle>
-
-        {tabs.map((tab) => (
-          <TabStyle
-            className={classnames("tab", { "is-active": filter === tab })}
-            key={tab}
-            onClick={() => setFilter(tab)}
-          >
-            {tab}
-          </TabStyle>
-        ))}
-      </TabsContainerStyle>
-
-      <Drop>
-        <Dropdown
-          controlClassName="customControlClass"
-          menuClassName="customMenuClass"
-          placeholderClassName="customPlaceHolderClass"
-          arrowOpen={<span className="arrowOpenClass" />}
-          arrowClosed={<span className="arrowClosedClass" />}
-          options={dropdownOptions}
-          value={defaultOption}
-          onChange={(e) => setFilter(e.label)}
-        />
-      </Drop>
-    </div>
-  );
-}
 
 // Styles
 const TabsContainerStyle = styled.div`
@@ -94,61 +38,47 @@ export const TabStyle = styled.div`
   }
 `;
 
-const Drop = styled.div`
-  padding: 10px;
+// Tabs are replaced with Select dropdown on mobile
+const SelectWrapper = styled.div`
+  width: 150px;
+  padding-bottom: 10px;
   @media (${minWidth(BREAKPOINT_MOBILE)}) {
     display: none;
   }
-
-  /* Custom styles that are passed as props to react-dropdown component */
-  .customControlClass {
-    display: flex;
-    justify-content: space-between;
-  }
-  .customControlClass,
-  .customMenuClass {
-    background-color: ${PURPLE_DARK};
-    color: ${GREY_4};
-    font-family: ${POPPINS};
-    font-size: 13px;
-    font-weight: 500;
-    line-height: 20px;
-    cursor: pointer;
-  }
-
-  .customPlaceHolderClass {
-    color: ${WHITE_PRIMARY};
-  }
-
-  .option {
-    color: inherit;
-    margin-top: 5px;
-    margin-bottom: 5px;
-  }
-
-  .option:hover {
-    background-color: inherit;
-    color: ${WHITE_PRIMARY};
-  }
-
-  .option.is-selected {
-    background-color: inherit;
-    color: ${WHITE_PRIMARY};
-  }
-
-  .arrowOpenClass,
-  .arrowClosedClass {
-    display: block;
-    content: " ";
-    padding: 10px;
-    width: auto;
-  }
-
-  .arrowOpenClass {
-    background: url(${arrowUp}) no-repeat center;
-  }
-
-  .arrowClosedClass {
-    background: url(${arrowDown}) no-repeat center;
-  }
 `;
+
+type Props = {
+  tabs: any;
+  filter: any;
+  setFilter: any;
+};
+
+export default function FilterTabs({ tabs, filter, setFilter }: Props) {
+  return (
+    <div>
+      <TabsContainerStyle>
+        {tabs.map((tab) => (
+          <TabStyle
+            className={classnames("tab", {
+              "is-active": filter === tab,
+            })}
+            key={tab.value}
+            onClick={() => {
+              setFilter(tab);
+            }}
+          >
+            {tab.label}
+          </TabStyle>
+        ))}
+      </TabsContainerStyle>
+
+      <SelectWrapper>
+        <Select
+          options={tabs}
+          selectedValue={filter}
+          setSelectedValue={setFilter}
+        />
+      </SelectWrapper>
+    </div>
+  );
+}
--- stories/base/FilterTabs.stories.tsx
+++ stories/base/FilterTabs.stories.tsx
@@ -5,23 +5,34 @@ import styled from "@emotion/styled";
 import FilterTabs from "../../components/base/FilterTabs";
 import { PURPLE_DARK } from "../../components/colors";
 
+const DarkBackground = styled.div`
+  background: ${PURPLE_DARK};
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// Filters to display as tabs
+const TABS = [
+  { value: "all", label: "ALL" },
+  { value: "get-building", label: "GET BUILDING" },
+  { value: "prerequisites", label: "PREREQUISITES" },
+  { value: "features", label: "FEATURES" },
+];
+
 storiesOf("base|FilterTabs", module)
   .addDecorator(centered)
   .addParameters({ backgrounds: [{ name: "dark", value: PURPLE_DARK }] })
   .add("default", () => {
-    const [filter, setFilter] = React.useState(null);
+    const [filter, setFilter] = React.useState(TABS[0]);
     return (
-      <BackgroundStyle>
-        <FilterTabs
-          tabs={["GET BUILDING", "PREREQUISITES", "FEATURES"]}
-          filter={filter}
-          setFilter={setFilter}
-        />
-      </BackgroundStyle>
+      <DarkBackground>
+        <FilterTabs tabs={TABS} filter={filter} setFilter={setFilter} />
+      </DarkBackground>
     );
   });
-
-const BackgroundStyle = styled.div`
-  background: ${PURPLE_DARK};
-  width: 414px;
-`;
