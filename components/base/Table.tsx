d39a780adcae2b61666c2942d04f4dd9aead6287
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-04-22 09:09:29 -0500

    Added table component with addressed comments (#37)

---
+++ components/base/Table.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { ROBOTO } from "../typography";
+import { HEADLINE_TEXT, PARAGRAPH_TEXT, GREY_1, GREY_2 } from "../colors";
+
+// Styles for Table
+const StyledTable = styled.div`
+  overflow-x: scroll;
+  white-space: nowrap;
+  table {
+    /* Separate borders to selectively apply border radius */
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0;
+    text-align: left;
+    font-family: ${ROBOTO};
+  }
+  th {
+    color: ${HEADLINE_TEXT};
+    font-size: 13px;
+    font-weight: 500;
+    line-height: 17px;
+    padding: 11px 21px;
+  }
+  td {
+    color: ${PARAGRAPH_TEXT};
+    font-size: 14px;
+    line-height: 20px;
+    padding: 17px 21px;
+    border-top: 1px solid ${GREY_2};
+    border-bottom: 1px solid ${GREY_2};
+    :nth-of-type(1) {
+      border-left: 1px solid ${GREY_2};
+    }
+    :nth-last-of-type(1) {
+      border-right: 1px solid ${GREY_2};
+    }
+  }
+  /* Applying background-color to alternate rows  */
+  tr:nth-of-type(even) {
+    background-color: ${GREY_1};
+  }
+  /* Top-left td cell */
+  tr:nth-of-type(2) td:nth-of-type(1) {
+    border-radius: 5px 0 0 0;
+  }
+  /* Top-right td cell */
+  tr:nth-of-type(2) td:nth-last-of-type(1) {
+    border-radius: 0 5px 0 0;
+  }
+  /* Bottom-right td cell */
+  tr:nth-last-of-type(1) td:nth-last-of-type(1) {
+    border-radius: 0 0 5px 0;
+  }
+  /* Bottom-left td cell */
+  tr:nth-last-of-type(1) td:nth-of-type(1) {
+    border-radius: 0 0 0 5px;
+  }
+`;
+
+// Prop types
+type Props = {
+  children: JSX.Element;
+};
+
+// Table component
+export default function Table({ children }: Props) {
+  return <StyledTable>{children}</StyledTable>;
+}
---
+++ stories/base/Table.stories.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+import { storiesOf } from "@storybook/react";
+import Table from "../../components/base/Table";
+import { InlineCode } from "../../components/base/Typography";
+
+const ParentDiv = styled.div`
+  padding: 50px;
+`;
+
+const tableData = (
+  <table>
+    <tr>
+      <th>Parameter</th>
+      <th>Required?</th>
+      <th>Type</th>
+      <th>Description</th>
+    </tr>
+    <tr>
+      <td>firstName</td>
+      <td>yes</td>
+      <td>string</td>
+      <td>Customer&apos;s first name</td>
+    </tr>
+    <tr>
+      <td>lastName</td>
+      <td>yes</td>
+      <td>string</td>
+      <td>Customer&apos;s last name</td>
+    </tr>
+    <tr>
+      <td>email</td>
+      <td>yes</td>
+      <td>string</td>
+      <td>Customer&apos;s last name</td>
+    </tr>
+    <tr>
+      <td>type</td>
+      <td>yes</td>
+      <td>string</td>
+      <td>
+        Value of <InlineCode>receive-only</InlineCode>
+      </td>
+    </tr>
+    <tr>
+      <td>businessName</td>
+      <td>yes</td>
+      <td>string</td>
+      <td>Customer&apos;s registered business name</td>
+    </tr>
+    <tr>
+      <td>ipAddress</td>
+      <td>yes</td>
+      <td>string</td>
+      <td>Customer&apos;s IP address</td>
+    </tr>
+  </table>
+);
+storiesOf("base|Table", module).add("default", () => (
+  <ParentDiv>
+    <Table>{tableData}</Table>
+  </ParentDiv>
+));
