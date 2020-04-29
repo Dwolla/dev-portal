3fc9d047f1e471e4bd7ff495ed3bb667be3b06c2
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-04-29 17:29:34 -0500

    Added Footer CTA | Updated Button paddings (#42)
    * Added Footer CTA | Updated Button paddings
    
    * Addressed margin/padding comments

---
+++ components/base/FooterCTA.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Button from "./Button";
+import { PURPLE_DARK } from "../colors";
+import { H2, SubHeader } from "./Typography";
+import { maxWidth, BREAKPOINT_IPAD, BREAKPOINT_MOBILE } from "../breakpoints";
+
+// Styles
+
+const Container = styled.div`
+  padding: 60px;
+  width: 100%;
+  background-color: ${PURPLE_DARK};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media (${maxWidth(BREAKPOINT_MOBILE)}) {
+    padding: 10px;
+    flex-direction: column;
+  }
+`;
+
+const StyledContent = styled.div`
+  padding-right: 20px;
+  > * {
+    margin: unset;
+    margin-bottom: 13px;
+  }
+  @media (${maxWidth(BREAKPOINT_MOBILE)}) {
+    padding-right: unset;
+    text-align: center;
+  }
+  /* Text sizes decrease in smaller screens */
+  @media (${maxWidth(BREAKPOINT_IPAD)}) {
+    > * {
+      margin-bottom: 7px;
+    }
+    > h2 {
+      font-size: 18px;
+      line-height: 22px;
+    }
+    > p {
+      font-size: 16px;
+      line-height: 20px;
+    }
+  }
+`;
+
+// Props
+type ButtonProps = {
+  text: string;
+};
+type Props = { topic: string; description: string; button?: ButtonProps };
+
+function FooterCTA({ topic, description, button }: Props) {
+  return (
+    <Container>
+      <StyledContent>
+        <H2 isDark>{topic}</H2>
+        <SubHeader isDark>{description}</SubHeader>
+      </StyledContent>
+      <Button {...button} size="large" variant="primary" />
+    </Container>
+  );
+}
+
+export default FooterCTA;
