8c3e100dd88ea0f4eff382ab5547680764a0942d
Stephen Ausman <stephen@dwolla.com>
2020-05-11 07:31:12 -0500

    Add CodeExamples component

---
+++ modules/highlight.ts
@@ -0,0 +1,20 @@
+import hljs from "highlight.js/lib/core";
+
+hljs.registerLanguage(
+  "javascript",
+  require("highlight.js/lib/languages/javascript")
+);
+
+hljs.registerLanguage("ruby", require("highlight.js/lib/languages/ruby"));
+
+hljs.registerLanguage("php", require("highlight.js/lib/languages/php"));
+
+hljs.registerLanguage("python", require("highlight.js/lib/languages/python"));
+
+hljs.registerLanguage("json", require("highlight.js/lib/languages/json"));
+
+hljs.registerLanguage("bash", require("highlight.js/lib/languages/bash"));
+
+export default function highlight(code: string, language: string) {
+  return hljs.highlight(language, code).value;
+}
