71c9ff1b1769934d6c4fdaa4b5025c9d5dac520a
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-05-12 10:53:23 -0500

    Added oarnge variant to Badge (#54)
    * Added oarnge variant to Badge
    
    * Addressed comments
    
    Co-authored-by: Shreya Thapa <sthapa@dwolla.com>

--- stories/base/Badge.stories.tsx
+++ stories/base/Badge.stories.tsx
@@ -5,4 +5,8 @@ import Badge from "../../components/base/Badge";
 
 storiesOf("base|Badge", module)
   .addDecorator(centered)
-  .add("default", () => <Badge text="BADGETEXT" />);
+  .add("default", () => <Badge text="BADGETEXT" />)
+  .add("with orange variant", () => <Badge text="3" variant="orange" />)
+  .add("with explicit default variant", () => (
+    <Badge text="default variation" variant="default" />
+  ));
