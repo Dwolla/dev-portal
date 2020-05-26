b4b70cabfda49c4623715cd124ce9a1a752759f9
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-05-26 17:02:36 -0500

    Added ExternalCTA component (#66)
    * Added ExternalCTA component
    
    * Addressed comments: added href prop to ExternalCTA card
    
    * Link opens in new tab, moved icon to content-images/content-icons
    
    Co-authored-by: Shreya Thapa <sthapa@dwolla.com>

---
+++ stories/base/ExternalCTA.stories.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import centered from "@storybook/addon-centered/react";
+import ExternalCTA from "../../components/base/ExternalCTA";
+import developerCommunityIcon from "../../assets/images/content-images/content-icons/developer-community-icon.svg";
+
+storiesOf("base|External CTA", module)
+  .addDecorator(centered)
+  .add("Default", () => (
+    <ExternalCTA
+      icon={developerCommunityIcon}
+      title="Developer Support"
+      href="https://discuss.dwolla.com/"
+      description="Interact with the developer community and Dwolla developer support to find answers to your technical questions."
+    />
+  ))
+  .add("No title", () => (
+    <ExternalCTA
+      icon={developerCommunityIcon}
+      href="https://discuss.dwolla.com/"
+      description="Interact with the developer community and Dwolla developer support to find answers to your technical questions."
+    />
+  ));
