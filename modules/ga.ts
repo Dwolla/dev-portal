394f16b4f042bf40c955da06095b42eb12eb0187
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-06-17 11:52:25 -0500

    Updated R08 return code actions table (#101)
    Co-authored-by: Shreya Thapa <sthapa@dwolla.com>

--- modules/ga.ts
+++ modules/ga.ts
@@ -2,6 +2,7 @@ export default function ga(...args) {
   if ("ga" in window) {
     window.ga(...args);
   } else {
+    // eslint-disable-next-line no-console
     console.log("window.ga not available.", JSON.stringify(args, null, 2));
   }
 }
