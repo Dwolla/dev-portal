52e76744e7cad9c3ebbce4df77ccacd4ee611623
ShreyaThapa <44006085+ShreyaThapa@users.noreply.github.com>
2020-06-18 15:11:21 -0500

    Added link prop to inlineCTA component (#97)
    Co-authored-by: Shreya Thapa <sthapa@dwolla.com>

--- stories/base/InlineCTA.stories.tsx
+++ stories/base/InlineCTA.stories.tsx
@@ -3,12 +3,22 @@ import { storiesOf } from "@storybook/react";
 import centered from "@storybook/addon-centered/react";
 import InlineCTA from "../../components/base/InlineCTA";
 import guideIcon from "../../assets/images/content-images/content-icons/guides-icon.svg";
+import conceptIcon from "../../assets/images/content-images/content-icons/concepts-icon.svg";
 
 storiesOf("base|Inline CTA", module)
   .addDecorator(centered)
   .add("Default", () => (
-    <InlineCTA
-      icon={guideIcon}
-      text="To learn more about how to initiate a Same-Day ACH credit transfer, reference our developer resource article."
-    />
+    <>
+      <InlineCTA
+        icon={conceptIcon}
+        text="To learn more about how to initiate a Same-Day ACH credit transfer, reference our developer concept article."
+        href="https://developers.dwolla.com/concepts/same-day-ach"
+      />
+      <br />
+      <InlineCTA
+        icon={guideIcon}
+        text="For more information on creating a Verified Personal Customer, reference our step by step guide."
+        href="https://developers.dwolla.com/guides/personal-verified-customer"
+      />
+    </>
   ));
