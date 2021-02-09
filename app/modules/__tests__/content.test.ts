fa267c1cff6a93db106800f357443ee31d6bb9c4
Stephen Ausman <stephen@dwolla.com>
2021-02-09 10:13:36 -0600

    Add Content module

---
+++ app/modules/__tests__/content.test.ts
@@ -0,0 +1,14 @@
+import { buildContentModule } from "../content";
+
+const TestContent = buildContentModule(`${__dirname}/fake-content/`);
+const TestContent2 = buildContentModule(`${__dirname}/fake-content`);
+
+test("list", async () => {
+  const content = await TestContent.list();
+  expect(content).toMatchSnapshot();
+});
+
+test("list (2)", async () => {
+  const content = await TestContent2.list();
+  expect(content).toMatchSnapshot();
+});
