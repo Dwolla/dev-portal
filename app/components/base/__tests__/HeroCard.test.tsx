067291716027016aa4b63c5528146b38e32ba9cf
Shreya <44006085+ShreyaThapa@users.noreply.github.com>
2021-06-18 11:31:43 -0500

    Add HeroCard component + test (#212)
    * Add HeroCard component + test
    
    * Updated test

---
+++ app/components/base/__tests__/HeroCard.test.tsx
@@ -0,0 +1,88 @@
+import renderer from "react-test-renderer";
+import HeroCard from "../HeroCard";
+
+test("Default HeroCard", () => {
+  const tree = renderer
+    .create(
+      <HeroCard
+        topic="Save development time with Drop-in Components"
+        description="Integrate with the Dwolla API with just a few lines of code "
+      />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test("HeroCard - with Button", () => {
+  const tree = renderer
+    .create(
+      <HeroCard
+        topic="Save development time with Drop-in Components"
+        description="Integrate with the Dwolla API with just a few lines of code "
+        button={{
+          text: "View Walkthrough Guide",
+          link: {
+            href: "https://www.dwolla.com",
+            external: false,
+          },
+        }}
+      />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test("HeroCard - with Links", () => {
+  const tree = renderer
+    .create(
+      <HeroCard
+        topic="Save development time with Drop-in Components"
+        description="Integrate with the Dwolla API with just a few lines of code "
+        links={[
+          {
+            text: "Technical Documentation",
+            href: "https://www.dwolla.com",
+          },
+          {
+            text: "Learn about the components",
+            href: "https://www.dwolla.com",
+          },
+        ]}
+      />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test("HeroCard - with Button and Links", () => {
+  const tree = renderer
+    .create(
+      <HeroCard
+        topic="Save development time with Drop-in Components"
+        description="Integrate with the Dwolla API with just a few lines of code "
+        button={{
+          text: "View Walkthrough Guide",
+          link: {
+            href: "https://www.dwolla.com",
+            external: false,
+          },
+        }}
+        links={[
+          {
+            text: "Technical Documentation",
+            href: "https://www.dwolla.com",
+          },
+          {
+            text: "Learn about the components",
+            href: "https://www.dwolla.com",
+          },
+        ]}
+      />
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
