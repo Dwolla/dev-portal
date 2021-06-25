a2c80e944a75f14714345133ef2fc996c882d4ec
Kelly Moreira <kmoreira@dwolla.com>
2021-06-25 14:24:30 -0500

    hero draft pr

---
+++ app/components/base/Hero.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+import { breakDown } from "../breakpoints";
+import hero from "../../../assets/images/content-images/hero.png";
+
+const StyledHero = styled.div`
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: right;
+  position: relative;
+  width: 100%;
+
+  @media (${breakDown("sm")}) {
+    display: none;
+  }
+
+  @media (${breakDown("xs")}) {
+    display: none;
+  }
+`;
+
+function Hero() {
+  return (
+    <>
+      <StyledHero>
+        <img src={hero} alt="hero-banner" />
+      </StyledHero>
+    </>
+  );
+}
+
+export default Hero;
