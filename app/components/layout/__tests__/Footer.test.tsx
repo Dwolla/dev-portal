3328d73d629d4ab64901275eff5994da20092065
Kelly Moreira <kmoreira@kmoreiramac.corp.dwolla.com>
2021-11-19 12:56:44 -0600

    Ctrl + F the project to find and update all instances of docs.dwolla.com

--- app/components/layout/__tests__/Footer.test.tsx
+++ app/components/layout/__tests__/Footer.test.tsx
@@ -18,7 +18,10 @@ test("Footer", () => {
       { text: "Integrations", href: "https://www.dwolla.com/integrations/" },
     ],
     Developers: [
-      { text: "API Reference", href: "https://docs.dwolla.com/" },
+      {
+        text: "API Reference",
+        href: "https://developers.dwolla.com/api-reference/",
+      },
       { text: "Support Forum", href: "https://discuss.dwolla.com/" },
       { text: "SDKs and Tools", href: "#sdks-and-tools" },
       { text: "Github Repos", href: "https://github.com/Dwolla/" },
