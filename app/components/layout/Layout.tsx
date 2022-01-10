6c8e777f9ebe888c53f887c280eadd4fe52eafb4
Shreya <44006085+ShreyaThapa@users.noreply.github.com>
2022-01-10 09:59:09 -0600

    DEV-788: Update Footer (#287)

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -176,10 +176,7 @@ export default function Layout({
   footerLinks: Record<string, FooterLink[]>;
   footerLegal: {
     title: string;
-    operatorDescription: string;
-    veridianDescription: string;
-    metaBankDescription: JSX.Element;
-    rtpDescription: string;
+    description: any;
   };
   topBarProps: TopBarProps;
   apiStatus: APIStatus;
