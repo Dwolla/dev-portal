56fab158568c0a683f031abfc20e873765aa8ff2
James Coon <james@jcoon.dev>
2022-04-11 19:35:41 -0400

    DEV-867: Upgrade Next.js to v9.5.5 and next-mdx-enhanced to v4.0.0

--- layouts/guides.tsx
+++ layouts/guides.tsx
@@ -4,6 +4,7 @@ import Pages from "../app/modules/pages";
 import Pagination from "../app/components/partial/NextBackPagination";
 import FooterCTA from "../app/components/base/FooterCTA";
 import { breakUp } from "../app/components/breakpoints";
+import { Props } from "./types";
 
 const StyledPagination = styled.div`
   padding: 20px;
@@ -17,48 +18,45 @@ const StyledPagination = styled.div`
   }
 `;
 
-export default (frontMatter) => {
-  // eslint-disable-next-line func-names
-  return function ({ children: content }: { children: any }) {
-    const splitPath = frontMatter.__resourcePath.split("/"); // eslint-disable-line no-underscore-dangle
-    splitPath.pop();
-    const parentPath = splitPath.join("/");
-    const prevPage = Pages.under(parentPath).find(
-      (page) => page.guide.step === frontMatter.guide.step - 1
-    );
-    const nextPage = Pages.under(parentPath).find(
-      (page) => page.guide.step === frontMatter.guide.step + 1
-    );
+export default function GuidesLayout({ children, frontMatter }: Props) {
+  const splitPath = frontMatter.__resourcePath.split("/"); // eslint-disable-line no-underscore-dangle
+  splitPath.pop();
+  const parentPath = splitPath.join("/");
+  const prevPage = Pages.under(parentPath).find(
+    (page) => page.guide.step === frontMatter.guide.step - 1
+  );
+  const nextPage = Pages.under(parentPath).find(
+    (page) => page.guide.step === frontMatter.guide.step + 1
+  );
 
-    return (
-      <>
-        <DefaultMDXWrapper frontMatter={frontMatter}>
-          {content}
-        </DefaultMDXWrapper>
+  return (
+    <>
+      <DefaultMDXWrapper frontMatter={frontMatter}>
+        {children}
+      </DefaultMDXWrapper>
 
-        {(!!prevPage || !!nextPage) && (
-          <StyledPagination>
-            <Pagination
-              prevTitle={prevPage ? prevPage.title : ""}
-              prevHref={prevPage ? prevPage.id : ""}
-              nextTitle={nextPage ? nextPage.title : ""}
-              nextHref={nextPage ? nextPage.id : ""}
-            />
-          </StyledPagination>
-        )}
+      {(!!prevPage || !!nextPage) && (
+        <StyledPagination>
+          <Pagination
+            prevTitle={prevPage ? prevPage.title : ""}
+            prevHref={prevPage ? prevPage.id : ""}
+            nextTitle={nextPage ? nextPage.title : ""}
+            nextHref={nextPage ? nextPage.id : ""}
+          />
+        </StyledPagination>
+      )}
 
-        <FooterCTA
-          topic="Test in the Sandbox for free today."
-          description="Use sandbox environment to test API requests."
-          button={{
-            text: "Get API Keys",
-            link: {
-              href: "https://accounts-sandbox.dwolla.com/sign-up",
-              external: true,
-            },
-          }}
-        />
-      </>
-    );
-  };
-};
+      <FooterCTA
+        topic="Test in the Sandbox for free today."
+        description="Use sandbox environment to test API requests."
+        button={{
+          text: "Get API Keys",
+          link: {
+            href: "https://accounts-sandbox.dwolla.com/sign-up",
+            external: true,
+          },
+        }}
+      />
+    </>
+  );
+}
