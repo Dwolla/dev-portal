f7e1b374c660e1089590198858e4f90aa475b2f1
James Coon <james@jcoon.dev>
2022-04-27 16:14:54 -0400

    Upgrade Next to v11

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-// next.config.js
-const withMdxEnhanced = require("next-mdx-enhanced");
+const withDwollaMdx = require("dwolla-mdx-remark");
 const withImages = require("next-images");
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 const withYaml = require("next-plugin-yaml");
@@ -7,21 +6,20 @@ const withYaml = require("next-plugin-yaml");
 module.exports = (phase) =>
   withYaml(
     withImages(
-      withMdxEnhanced({
-        layoutPath: "layouts",
-        defaultLayout: true,
-        // fileExtensions: ['mdx'],
-        // remarkPlugins: [],
-        // rehypePlugins: [],
-        // extendFrontMatter: {
-        //   process: (mdxContent, frontMatter) => {},
-        //   phase: 'prebuild|loader|both'
-        // }
+      withDwollaMdx({
+        extension: /\.mdx?$/,
+        options: {
+          providerImportSource: "@mdx-js/react",
+        },
       })({
         target: "serverless",
         env: {
           isDev: phase === PHASE_DEVELOPMENT_SERVER,
         },
+        images: {
+          disableStaticImages: true,
+        },
+        pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
         webpack: (config) => {
           config.module.rules.unshift({
             test: /\.svg$/,
