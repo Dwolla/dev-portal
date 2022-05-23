03661e95bc46984c5eb194aede473d991e4b97f2
James Coon <james@jcoon.dev>
2022-05-23 13:40:26 -0400

    Storybook now includes Emotion support by default

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,6 @@
 const path = require("path");
 const fs = require("fs");
 
-function getPackageDir(filepath) {
-  let currDir = path.dirname(require.resolve(filepath));
-  while (true) {
-    if (fs.existsSync(path.join(currDir, "package.json"))) {
-      return currDir;
-    }
-    const { dir, root } = path.parse(currDir);
-    if (dir === root) {
-      throw new Error(
-        `Could not find package.json in the parent directories starting from ${filepath}.`
-      );
-    }
-    currDir = dir;
-  }
-}
-
 module.exports = {
   core: {
     builder: "webpack5",
@@ -29,11 +13,4 @@ module.exports = {
     "@storybook/addon-backgrounds",
   ],
   framework: "@storybook/react",
-  // create alias to resolve emotion modules that were renamed in emotion 11
-  webpackFinal: async (config) => {
-    config.resolve.alias["@emotion/core"] = getPackageDir("@emotion/react");
-    config.resolve.alias["@emotion/styled"] = getPackageDir("@emotion/styled");
-    config.resolve.alias["emotion-theming"] = getPackageDir("@emotion/react");
-    return config;
-  },
 };
