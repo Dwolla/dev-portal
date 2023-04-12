28a65ab0a85b9d80fe45638d6d5df8c7a8642f82
James Coon <james@jcoon.dev>
2023-04-12 10:55:12 -0600

    DEV-1365: Merge Algolia branch into main (#479)

--- stories/layout/TopBar.stories.tsx
+++ stories/layout/TopBar.stories.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { storiesOf } from "@storybook/react";
+import algoliasearch from "algoliasearch/lite";
 import TopBar from "../../app/components/layout/TopBar";
 import { LanguageContext } from "../../app/components/util/Contexts";
 
@@ -74,4 +75,24 @@ storiesOf("layout/TopBar", module)
       />
       <LangaugeContextAccessor />
     </>
+  ))
+  .add("with global search form", () => (
+    <TopBar
+      algoliaSearch={{
+        branch: "main-algolia",
+        searchClient: algoliasearch(
+          "L2PPGO4SBB",
+          "6a6c05b578da5aa729df7f53776e9f76"
+        ),
+        siteId: "e19df9e6-7024-443d-8ec0-26e8312ce0f9",
+        searchOptions: { hitsPerPage: 5 },
+      }}
+      button={{
+        text: "Get API Keys",
+        link: {
+          href: "https://accounts-sandbox.dwolla.com/sign-up",
+          external: true,
+        },
+      }}
+    />
   ));
