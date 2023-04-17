9ebcc85b12b9d3cc2ce96d8e0bf331f6a6bcad6a
James Coon <james@jcoon.dev>
2023-04-17 15:03:11 -0600

    Update Babel and Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,26 @@
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   // import all files ending in *.stories.tsx or *.stories.mdx
-  stories: ["../stories/**/*.stories.@(tsx|mdx)"],
   addons: [
     "@storybook/addon-viewport/register",
     "@storybook/addon-postcss",
     "@storybook/addon-backgrounds",
   ],
-  framework: "@storybook/react",
+  docs: {
+    autodocs: true,
+  },
+  features: {
+    storyStoreV7: false,
+  },
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  stories: ["../stories/**/*.stories.@(tsx|mdx)"],
   webpackFinal: (config) => {
     const fileLoaderRule = config.module.rules.find(
       (rule) => rule.test && rule.test.test(".svg")
     );
     fileLoaderRule.exclude = /\.svg$/;
-
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack", "url-loader"],
