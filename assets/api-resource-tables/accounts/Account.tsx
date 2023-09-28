d98418c5a54c4c4a753daa6a411c7b0e70a351dd
Shreya <44006085+ShreyaThapa@users.noreply.github.com>
2023-09-28 09:30:03 -0500

    Dev 1446: Add Connect API Reference (#544)
    Co-authored-by: James Coon <james@jcoon.dev>
    Co-authored-by: Spencer Hunter <spencer.j.hunter@gmail.com>

---
+++ assets/api-resource-tables/accounts/Account.tsx
@@ -0,0 +1,40 @@
+import { type TableContents } from "../../../app/components/base/material-ui/Table";
+import HalLink from "../HalLink";
+import { Link } from "../../../app/components/base/Typography";
+
+export default {
+  headers: ["Property", "Type", "Description"],
+  rows: [
+    [
+      [
+        "_links",
+        "object",
+        <span>
+          <Link href="/api-reference#links" text="HAL-JSON" /> links to related
+          resources.
+        </span>,
+      ],
+      {
+        title: "Account Links Object",
+        headers: ["Link", "Description"],
+        rows: [
+          [
+            ["self", "References the current Account resource."],
+            HalLink("account"),
+          ],
+        ],
+      },
+    ],
+    [
+      "id",
+      "string <uuid>",
+      "Unique identifier of Account that is assigned by Dwolla.",
+    ],
+    ["name", "string", "Name of the Account provided upon creation."],
+    [
+      "created",
+      "string <date-time>",
+      "ISO-8601 timestamp of when the resource was created.",
+    ],
+  ],
+} as TableContents;
